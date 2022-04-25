df8365a2150088099ede3dadb8445bfd01eb09bf
Juan-cyber1 <juan.vanegasg@cun.edu.co>
2022-04-25 09:16:54 -0500

    autoplay carrusel

--- js/main.js
+++ js/main.js
@@ -53,3 +53,42 @@ const countDownClock = (number = 100, format = 'seconds') => {
   days, hours, minutes or seconds
 */
 countDownClock(26, 'days');
+
+
+/* Carrusel negocios */
+
+$('.carrusel-icons').slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    arrows:true,
+    slidesToShow: 8,
+    autoplay:true,
+    infinite:true,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
