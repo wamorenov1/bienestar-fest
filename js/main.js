8dd5bf39b65e1e29ea34c6bed822b68b340a163a
Juan-cyber1 <juan.vanegasg@cun.edu.co>
2022-04-28 14:49:22 -0500

    se añaden imagenes carrusel y funcionalidad con modales

--- js/main.js
+++ js/main.js
@@ -47,6 +47,41 @@ var x = setInterval(function () {
 
 /* Carrusel negocios */
 
+$('.carrusel-proms').slick({
+  dots: false,
+  infinite: false,
+  speed: 300,
+  arrows: false,
+  slidesToShow: 8,
+  autoplay: true,
+  infinite: true,
+  slidesToScroll: 1,
+  responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+
 $('.carrusel-icons').slick({
   dots: false,
   infinite: false,
